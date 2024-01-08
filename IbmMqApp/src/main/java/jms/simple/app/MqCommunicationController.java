15936ee712e4282d0d7a16c044f63ae85cdec2cd
kleonova <kleonova0@gmail.com>
2024-01-08 23:59:49 +0800

    add ibm mq

---
+++ IbmMqApp/src/main/java/jms/simple/app/MqCommunicationController.java
@@ -0,0 +1,27 @@
+package jms.simple.app;
+
+import jakarta.jms.JMSException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+@RequiredArgsConstructor
+public class MqCommunicationController {
+
+    private final MQCommunication mqCommunication;
+
+    @GetMapping("save")
+    public void saveStringInMq(@RequestBody String stringToSaveInMq) {
+        System.out.println(stringToSaveInMq);
+        mqCommunication.sendMessageToMQ(stringToSaveInMq);
+    }
+
+    @GetMapping("read")
+    public String readStringFromMQ() throws JMSException {
+        return mqCommunication.readMessageFromMq();
+    }
+}
